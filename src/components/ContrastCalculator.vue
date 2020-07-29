a3df546ca371239362e3e1065fd9f69155edb344
Achilles Moraites <achimoraites@yahoo.gr>
2020-07-29 21:58:18 +0300

    set coming soon

---
+++ src/components/ContrastCalculator.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  <h1>{{ test }}</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'ContrastCalculator',
+  props: {
+    msg: String
+  },
+    data: () => ({
+    test: 'Constrast Calculator'
+  })
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
